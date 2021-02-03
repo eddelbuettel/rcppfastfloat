b07f790d821e612c852f6e9f1c661c8bb58ab321
Brendan Knapp <brendan.g.knapp@gmail.com>
2021-02-03 10:54:29 -0800

    New function (as.double2), plus documentation and tests.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,22 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
+// as_dbl
+Rcpp::DoubleVector as_dbl(const Rcpp::CharacterVector x);
+RcppExport SEXP _RcppFastFloat_as_dbl(SEXP xSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< const Rcpp::CharacterVector >::type x(xSEXP);
+    rcpp_result_gen = Rcpp::wrap(as_dbl(x));
+    return rcpp_result_gen;
+END_RCPP
+}
 // parseExample
 double parseExample(const std::string& input, bool verbose);
 RcppExport SEXP _RcppFastFloat_parseExample(SEXP inputSEXP, SEXP verboseSEXP) {
@@ -19,6 +35,7 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_RcppFastFloat_as_dbl", (DL_FUNC) &_RcppFastFloat_as_dbl, 1},
     {"_RcppFastFloat_parseExample", (DL_FUNC) &_RcppFastFloat_parseExample, 2},
     {NULL, NULL, 0}
 };
